b073d95fd2b07a7f6365927c69ad9f821fa1276b
santuan <smrcouto88@gmail.com>
2025-06-01 00:14:23 -0300

    pp splash screen

--- src/components/ui/SplashScreen.vue
+++ src/components/ui/SplashScreen.vue
@@ -28,15 +28,15 @@ function focusOnTitleEsc(e: { keyCode: number }) {
 <template>
   <div
     role="alert"
-    class="absolute top-0 m-1 right-0 overflow-hidden flex-col flex justify-center items-center z-60 pointer-events-none"
+    class="absolute ring-2 ring-primary w-40 sm:w-auto top-0.5 m-1 right-0.5 text-right flex-col flex justify-center items-center z-60 pointer-events-none"
   >
-    <div class="p-2 h-10 relative z-10 text-xs text-primary-foreground flex justify-center items-center bg-primary">
+    <div class="p-2  relative z-10 text-xs text-foreground flex justify-center items-center bg-secondary">
       {{ t('message.documentNotSaved') }}
     </div>
   </div>
   <button
-    :class="document.show_sidebar_documents ? 'left-80 ' : ' ml-12'"
-    class="m-1 fixed ring-2 ring-primary  flex justify-center items-center inset-0"
+    :class="document.show_sidebar_documents ? 'left-80 ' : ' ml-0'"
+    class="m-1 mb-0 fixed ring-2 ring-primary  flex justify-center items-center inset-0"
     @click="focusOnTitle()" @keydown="focusOnTitleEsc($event)"
   >
     <span class="sr-only">{{ t('message.clickToEdit') }}</span>
